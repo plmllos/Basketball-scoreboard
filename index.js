8bccc7e11a12dcfc7721a00a261461306cca4834
Milos Popovic <popovic.milos@live.com>
2023-04-29 17:12:42 +0200

    Made displayValue() and displayFoul() functions reusable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let homeScore = 0
-let guestScore = 0
+let scoreHome = 0
+let scoreGuest = 0
 let foulHome = 0
 let foulGuest = 0
 
@@ -8,97 +8,96 @@ let guestScoreEl = document.getElementById("guest-score")
 let foulHomeEl = document.getElementById("foul-home")
 let foulGuestEl = document.getElementById("foul-guest")
 
-function increment1() {
-    homeScore += 1
-    if (homeScore < 10) {
-        homeScoreEl.textContent = "00" + homeScore
-    } else if (homeScore >= 10 && homeScore < 100) {
-        homeScoreEl.textContent = "0" + homeScore
+let increment1 = document.getElementById("increment-btn1")
+let increment2 = document.getElementById("increment-btn2")
+let increment3 = document.getElementById("increment-btn3")
+let increment4 = document.getElementById("increment-btn4")
+let increment5 = document.getElementById("increment-btn5")
+let increment6 = document.getElementById("increment-btn6")
+let increment7 = document.getElementById("increment-btn7")
+let increment8 = document.getElementById("increment-btn8")
+let newGame = document.getElementById("new-btn")
+
+increment1.addEventListener("click", function() {
+    incrementValue1(1)
+    displayValue(homeScoreEl, scoreHome)
+})
+
+increment2.addEventListener("click", function() {
+    incrementValue1(2)
+    displayValue(homeScoreEl, scoreHome)
+})
+
+increment3.addEventListener("click", function() {
+    incrementValue1(3)
+    displayValue(homeScoreEl, scoreHome)
+})
+
+increment4.addEventListener("click", function() {
+    incrementValue2(1)
+    displayValue(guestScoreEl, scoreGuest)
+})
+
+increment5.addEventListener("click", function() {
+    incrementValue2(2)
+    displayValue(guestScoreEl, scoreGuest)
+})
+
+increment6.addEventListener("click", function() {
+    incrementValue2(3)
+    displayValue(guestScoreEl, scoreGuest)
+})
+
+function incrementValue1(num1) {
+    scoreHome += num1
+}
+function incrementValue2(num2) {
+    scoreGuest += num2
+}
+
+function displayValue(display, score) {
+    if (score < 10) {
+        display.textContent = "00" + score
+    } else if (score >= 10 && score < 100) {
+        display.textContent = "0" + score
     } else {
-        homeScoreEl.textContent = homeScore
+        display.textContent = score
     }
 }
 
-function increment2() {
-    homeScore += 2
-    if (homeScore < 10) {
-        homeScoreEl.textContent = "00" + homeScore
-    } else if (homeScore >= 10 && homeScore < 100) {
-        homeScoreEl.textContent = "0" + homeScore
-    } else {
-        homeScoreEl.textContent = homeScore
-    }
+increment7.addEventListener("click", function() {
+    incrementValue3(1)
+    displayFoul(foulHomeEl, foulHome)
+})
+
+increment8.addEventListener("click", function() {
+    incrementValue4(1)
+    displayFoul(foulGuestEl, foulGuest)
+})
+
+function incrementValue3(num3) {
+    foulHome += num3
 }
 
-function increment3() {
-    homeScore += 3
-    if (homeScore < 10) {
-        homeScoreEl.textContent = "00" + homeScore
-    } else if (homeScore >= 10 && homeScore < 100) {
-        homeScoreEl.textContent = "0" + homeScore
-    } else {
-        homeScoreEl.textContent = homeScore
-    }
+function incrementValue4(num4) {
+    foulGuest += num4
 }
 
-function increment4() {
-    guestScore += 1
-    if (guestScore < 10) {
-        guestScoreEl.textContent = "00" + guestScore
-    } else if (guestScore >= 10 && guestScore < 100) {
-        guestScoreEl.textContent = "0" + guestScore
+function displayFoul(displayF, scoreF) {
+    if (scoreF < 10) {
+        displayF.textContent = "0" + scoreF
     } else {
-        guestScoreEl.textContent = guestScore
-    }
-}
-
-function increment5() {
-    guestScore += 2
-    if (guestScore < 10) {
-        guestScoreEl.textContent = "00" + guestScore
-    } else if (guestScore >= 10 && guestScore < 100) {
-        guestScoreEl.textContent = "0" + guestScore
-    } else {
-        guestScoreEl.textContent = guestScore
+        displayF.textContent = scoreF
         }
 }
 
-function increment6() {
-    guestScore += 3
-    if (guestScore < 10) {
-        guestScoreEl.textContent = "00" + guestScore
-    } else if (guestScore >= 10 && guestScore < 100) {
-        guestScoreEl.textContent = "0" + guestScore
-    } else {
-        guestScoreEl.textContent = guestScore
-        }
-}
-
-function incrementFaulHome() {
-    foulHome += 1
-    if (foulHome < 10) {
-        foulHomeEl.textContent = "0" + foulHome
-    } else {
-        foulHomeEl.textContent = foulHome
-        }
-}
-
-function incrementFaulGuest() {
-    foulGuest += 1
-    if (foulGuest < 10) {
-        foulGuestEl.textContent = "0" + foulGuest
-    } else {
-        foulGuestEl.textContent = foulGuest
-        }
-}
-
-function startNewGame() {
-    homeScore = 0
-    guestScore = 0
+newGame.addEventListener('click', function(){
+    scoreHome = 0
+    scoreGuest = 0
     foulHome = 0
     foulGuest = 0
     homeScoreEl.textContent = "000"
     guestScoreEl.textContent = "000"
     foulHomeEl.textContent = "00"
     foulGuestEl.textContent = "00"
-}
+})
